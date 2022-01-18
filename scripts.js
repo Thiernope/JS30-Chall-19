064930271f4b64eb6656f95b96b5971d9ba4eca2
Thierry Ntirandekura <56275083+Thiernope@users.noreply.github.com>
2022-01-18 15:10:00 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,6 +28,17 @@ function paintToCanvas() {
   
     setInterval(() => {
       ctx.drawImage(video, 0, 0, width, height);
+      // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    //pixels = redEffect(pixels);
+
+      pixels = rgbSplit(pixels);
+     //ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
 
     },16)
 }
@@ -48,4 +59,23 @@ function takePhoto() {
 
 }
 
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] - 100; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels; 
+  }
+
+
+  function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0]; // RED
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+    return pixels;
+  }
 video.addEventListener("canplay", paintToCanvas);
